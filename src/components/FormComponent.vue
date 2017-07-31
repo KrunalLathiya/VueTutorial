c4c90d56f2479a3f984b80cddf84cbc39be55da9
KrunalLathiya <krunallathiya21@gmail.com>
2017-07-31 23:33:15 +0530

    Simple Vue 2.x Tutorial

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+  <form>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <input class="input col-is-4" type="text" v-model="item"/>
+      </div>
+  </div>
+  <div class="field">
+    <div class="control">
+      <button class="button is-success" @click.prevent="addItems">Add</button>
+    </div>
+  </div>
+</form>
+
+<table v-if="items.length > 0" class="table is-striped">
+  <tr>
+    <th>Index</th>
+    <th>Items</th>
+    <th>Action</th>
+  </tr>
+  <tr v-for="(item, index) in items">
+    <td>{{ index }}</td>
+    <td>{{ item.message }}</td>
+    <button @click.prevent="deleteData(index)" class="button is-danger">Delete</button>
+  </tr>
+</table>
+
+</div>
+</template>
+<script>
+export default {
+  data: function(){
+    return {
+      item: '',
+      items: []
+    }
+  },
+  methods: {
+    addItems(){
+      this.items.push({message: document.querySelector('input').value});
+      this.item = '';
+    },
+    deleteData(index){
+      this.items.splice(index, 1);
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import FormComponent from './components/FormComponent.vue';
+
+new Vue({
+  el: '#app',
+  render: h => h(FormComponent)
+});
